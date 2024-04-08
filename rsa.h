6d2f6e3cc673217626c9d23b5b13f7f0d93dd539
Yermehyaw <yermehyaw@gmail.com>
2024-04-07 18:38:03 -0700

    Update header file

---
+++ rsa.h
@@ -0,0 +1,13 @@
+#ifndef RAS_H
+#define RAS_H
+
+/* Incluxde libs */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Prototypes */
+void factors(char *);
+
+
+#endif /*ifndef RSA_H */
